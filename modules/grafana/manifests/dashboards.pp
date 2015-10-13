b75b6da0cc26776a88bc20ef7c89446127a95c81
Alex Muller <alex.muller@digital.cabinet-office.gov.uk>
2015-10-13 14:30:45 +0100

    Remove duplication from Grafana templates
    These ERB templates have a lot of duplication in them. We can remove
    it by setting the variables in the Puppet manifest and using them
    in the templates.

--- modules/grafana/manifests/dashboards.pp
+++ modules/grafana/manifests/dashboards.pp
@@ -27,6 +27,65 @@ class grafana::dashboards (
     source  => 'puppet:///modules/grafana/dashboards',
   }
 
+  $common_intervals = [
+    'auto',
+    '1s',
+    '1m',
+    '5m',
+    '10m',
+    '30m',
+    '1h',
+    '3h',
+    '12h',
+    '1d',
+    '1w',
+    '1y',
+  ]
+
+  $common_nav = [
+    {
+      'type' => 'timepicker',
+      'collapse' => false,
+      'notice' => false,
+      'enable' => true,
+      'status' => 'Stable',
+      'time_options' => [
+        '5m',
+        '15m',
+        '1h',
+        '6h',
+        '12h',
+        '24h',
+        '2d',
+        '7d',
+        '30d',
+      ],
+      'refresh_intervals' => [
+        '5s',
+        '10s',
+        '30s',
+        '1m',
+        '5m',
+        '15m',
+        '30m',
+        '1h',
+        '2h',
+        '1d',
+      ],
+      'now' => true,
+    },
+  ]
+
+  $legend_options = {
+    'show' => true,
+    'values' => false,
+    'min' => false,
+    'max' => false,
+    'current' => false,
+    'total' => false,
+    'avg' => false,
+  }
+
   file {
     "${dashboard_directory}/2ndline_health.json": content => template('grafana/dashboards/2ndline_health.json.erb');
     "${dashboard_directory}/application_http_error_codes.json": content => template('grafana/dashboards/application_http_error_codes.json.erb');
