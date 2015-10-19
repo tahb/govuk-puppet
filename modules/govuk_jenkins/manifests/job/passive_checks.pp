66b13fbc3252d44ba047920940ecd13bbc0d3680
Paul Martin <Paul.martin@digital.cabinet-office.gov.uk>
2015-10-19 15:06:54 +0100

    Add Icinga service for passive checks
    The configuration and jobs were added to allow Icinga monitoring for
    specific high value Jenkins jobs. While the Jenkins side works
    correctly, Icinga unfortunately did not know what to do with the passive
    checks. I believe this is because the service is not defined anywhere.
    
    This commit adds in the defined passive checks on Icinga so that it can
    successfully receive the service.

--- modules/govuk_jenkins/manifests/job/passive_checks.pp
+++ modules/govuk_jenkins/manifests/job/passive_checks.pp
@@ -2,16 +2,37 @@
 #
 # Create two files on disk that can be parsed by jenkins-job-builder
 #
-class govuk_jenkins::job::passive_checks {
-  file { '/etc/jenkins_jobs/jobs/success_passive_check.yaml':
-    ensure  => present,
-    content => template('govuk_jenkins/jobs/success_passive_check.yaml.erb'),
-    notify  => Exec['jenkins_jobs_update'],
-  }
+# [*app_domain*]
+#   Domain to be used in link to Jenkins job.
+#
+class govuk_jenkins::job::passive_checks (
+  $app_domain = hiera('app_domain'),
+  ) {
+    file { '/etc/jenkins_jobs/jobs/success_passive_check.yaml':
+      ensure  => present,
+      content => template('govuk_jenkins/jobs/success_passive_check.yaml.erb'),
+      notify  => Exec['jenkins_jobs_update'],
+    }
 
-  file { '/etc/jenkins_jobs/jobs/failure_passive_check.yaml':
-    ensure  => present,
-    content => template('govuk_jenkins/jobs/failure_passive_check.yaml.erb'),
-    notify  => Exec['jenkins_jobs_update'],
+    $success_passive_check_url = "https://deploy.${app_domain}/job/Success_Passive_Check"
+
+    @@icinga::passive_check { "jenkins_job_success_${::hostname}":
+      service_description => 'Jenkins Job Success',
+      host_name           => $::fqdn,
+      action_url          => $success_passive_check_url,
+    }
+
+    $failure_passive_check_url = "https://deploy.${app_domain}/job/Failure_Passive_Check"
+
+    @@icinga::passive_check { "jenkins_job_failure_${::hostname}":
+      service_description => 'Jenkins Job Failure',
+      host_name           => $::fqdn,
+      action_url          => $failure_passive_check_url,
+    }
+
+    file { '/etc/jenkins_jobs/jobs/failure_passive_check.yaml':
+      ensure  => present,
+      content => template('govuk_jenkins/jobs/failure_passive_check.yaml.erb'),
+      notify  => Exec['jenkins_jobs_update'],
+    }
   }
-}
